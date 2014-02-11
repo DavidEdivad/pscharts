f7168a2fd1d715c0bc23f35d3a4ea45c18196820
davideedivad <dadolino@libero.it>
2014-02-11 18:30:32 +0100

    1mo commit

---
+++ MvcCharts/Models/DataClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MvcCharts.Models
+{
+    public class DataClass
+    {
+        public DateTime ExecutionDate { get; set; }
+        public double ExecutionValue { get; set; }
+    }
+}
